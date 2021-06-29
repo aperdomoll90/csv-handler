adef93d4879cc86a54aee46b0522c51d498bc638
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2021-06-29 10:36:33 -0400

    all functions created, able to read files, able to dedup the arrays and count the number or domains, creates both csv and json files

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const csv = require('csv-parser')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
-    path: 'results.csv',
+    path: 'deduped.csv',
     header: [
         {id: 'URL', title: 'URL'},
         {id: 'Age', title: 'Age'},
@@ -13,24 +13,40 @@ const csvWriter = createCsvWriter({
 const arr=[]
 const domainArr=[]
 
-
-fs.createReadStream('test.csv')
+fs.createReadStream('source.csv')
 .pipe(csv({}))
 .on('data',(data)=>arr.push(data))
-.on('data',(data)=>domainArr.push(data.URL))
+.on('data',(data)=>domainArr.push(data.URL.split('/')[2]))
 .on('end', () => {
-    const domainPart = domainArr.map(domain => { 
-        const parts = domain.split('/')
-        return parts[2]
-      })
-      console.log(domainPart)
-    let uniqUsers = [...arr.reduce((map, obj) => map.set(obj.URL,obj),new Map()).values()]
+    const uniqUsers = [...arr.reduce((map, obj) => map.set(obj.URL.split('/')[2],obj),new Map()).values()]
+    // This line iterates through the array using a higher order function.
+    // We pass a function through it with an accumulator value(map) and a variable to place the objects/iterations in (obj)
+    // Set a new empty Map as initial value
+    // By using .set to add to the accumulator any object that meets the key( in this case a parsed domain ) overwriting the one previously there if there was a duplicated
+
+    // creates an array of the counted domains for Json object
+    let domainCount = {}
+    for (var i = 0; i < domainArr.length; i++) {
+        domainCount[domainArr[i]] = domainCount[domainArr[i]] + 1 || 1 ;
+    }
     
+
+    //creates CSV file
     csvWriter.writeRecords(uniqUsers)    
     .then(() => {
-        console.log(uniqUsers,'...Done');
+        console.log(uniqUsers,'csv created');
     });
-    // console.log(uniqUsers)
+
+     //creates JSON file
+    const myJSON = JSON.stringify(domainCount);
+    fs.writeFile('./count.json', myJSON, err => {
+        if (err){
+            console.log(err)
+        }else {
+            console.log(myJSON,'json created');
+        }
+    })
+    
   })
 
  
